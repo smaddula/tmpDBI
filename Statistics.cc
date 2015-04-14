0ded55f49f67b71a29cc5c8558586d2032927f96
Sidddardha <siddardha.iiit@gmail.com>
2015-04-13 22:19:45 -0400

    removing using and some comments

--- Statistics.cc
+++ Statistics.cc
@@ -10,16 +10,11 @@
 #include <fstream>
 
 using namespace std;
-/*
-using std::string;
-using std::vector;
-using std::clog; using std::endl;
-*/
 Statistics::Statistics() 
  {
 }
 
-Statistics::Statistics(Statistics &copyMe __attribute__ ((__unused__))) 
+Statistics::Statistics(Statistics &copyMe ) 
  {
 }
 Statistics::~Statistics()
@@ -28,17 +23,6 @@ Statistics::~Statistics()
 
 void Statistics::AddRel(char *relName, int numTuples)
 {
-  // This operation adds another base relation into the structure. The
-  // parameter set tells the statistics object what the name and size
-  // of the new relation is (size is given in terms of the number of
-  // tuples).
-
-  // Note that AddRel can be called more than one time for the same
-  // relation or attribute. If this happens, then you simply update
-  // the number of tuples or number of distinct values for the
-  // specified attribute or relation.
-  using std::string;
-  // look up in map, insert
   string s(relName);
   const RelationInformation newRelation (numTuples, s);
   rels[s] = newRelation;
@@ -48,19 +32,6 @@ void Statistics::AddRel(char *relName, int numTuples)
 }
 void Statistics::AddAtt(char *relName, char *attName, int numDistincts)
 {
-  // This operation adds an attribute to one of the base relations in
-  // the structure. The parameter set tells the Statistics object what
-  // the name of the attribute is, what relation the attribute is
-  // attached to, and the number of distinct values that the relation
-  // has for that particular attribute. If numDistincts is initially
-  // passed in as a –1, then the number of distincts is assumed to be
-  // equal to the number of tuples in the associated relation.
-
-  // Note that AddAtt can be called more than one time for the same
-  // relation or attribute. If this happens, then you simply update
-  // the number of tuples or number of distinct values for the
-  // specified attribute or relation.
-  using std::string;
   string const rel(relName);
   string const att(attName);
   if (-1 == numDistincts)
@@ -71,24 +42,19 @@ void Statistics::AddAtt(char *relName, char *attName, int numDistincts)
     }
   else
     {
-      // relationInformation[rel] = make_pair(att, numDistincts);
       rels[rel].AddAtt(att,numDistincts);
-      // std::clog << "before set insert with numdistincts given" << std::endl;
-      // std::clog << "attempting to insert atter \"" << att << "\"" << std::endl;
       extantAttrs.insert(make_pair(att,rel));
-      // std::clog << "after set insert with numdistincts given" << std::endl;
     }
-  // rels[rel].print();
 }
 void Statistics::CopyRel(char *oldName, char *newName)
 {
-  std::string oldN(oldName);
-  std::string newN(newName);
-  std::map < std::string, tupleCount > const oldAttrs = rels[oldN].GetAtts();
+  string oldN(oldName);
+  string newN(newName);
+  map < std::string, tupleCount > const oldAttrs = rels[oldN].GetAtts();
 
   RelationInformation newR(rels[oldN].NumTuples(), oldN);
 
-  std::map < std::string, tupleCount >::const_iterator it;
+  map < std::string, tupleCount >::const_iterator it;
   for (it = oldAttrs.begin(); it != oldAttrs.end(); it++ )
     {
       string newAttrName(newN+"."+(*it).first); // might need to do this for the schma variables as well.
@@ -104,7 +70,6 @@ void Statistics::CopyRel(char *oldName, char *newName)
 void Statistics::Read(char *fromWhere)
 {
   //clog << endl;
-  using std::ifstream;
   ifstream statFile(fromWhere);
 
   if (!statFile.good())
@@ -142,12 +107,11 @@ void Statistics::Read(char *fromWhere)
 }
 void Statistics::Write(char *fromWhere)
 {
-  using std::ofstream;
   ofstream statFile(fromWhere);
 
   statFile << rels.size() << endl;
   {
-    std::map < std::string, RelationInformation >::iterator it;
+    map < std::string, RelationInformation >::iterator it;
     for (it = rels.begin(); it != rels.end(); it++ )
       {
         statFile << (*it).first << endl << (*it).second << endl;
@@ -155,7 +119,7 @@ void Statistics::Write(char *fromWhere)
   }
   statFile << extantAttrs.size() << endl;
   {
-    std::map < std::string, std::string>::iterator it;
+    map < std::string, std::string>::iterator it;
     for (it = extantAttrs.begin(); it != extantAttrs.end(); it++ )
       {
         statFile << (*it).first << endl << (*it).second << endl;
@@ -163,7 +127,7 @@ void Statistics::Write(char *fromWhere)
   }
   statFile << mergedRelations.size() << endl;
   {
-    std::map < std::string, std::string>::const_iterator it;
+    map < std::string, std::string>::const_iterator it;
     for (it = mergedRelations.begin(); it != mergedRelations.end(); it++ )
       {
         statFile << (*it).first << endl << (*it).second << endl;
@@ -175,48 +139,8 @@ void Statistics::Write(char *fromWhere)
 
 void  Statistics::Apply(struct AndList *parseTree, char *relNames[], int numToJoin)
 {
-  // This operation takes a bit of explanation. Internally within the
-  // Statistics object, the various relations are partitioned into a set of
-  // subsets or partitions, where each and every relation is contained within
-  // exactly one subset (initially, each relation is in its very own singleton
-  // subset). When two or more relations are within the same subset, it means
-  // that they have been “joined” and they do not exist independently
-  // anymore. The Apply operation uses the statistics stored by the Statistics
-  // class to simulate a join of all of the relations listed in the relNames
-  // parameter. This join is performed using the predicates listed in the
-  // parameter parseTree.
-
-  // Of course, the operation does not actually perform a join (actually
-  // performing a join will be the job of the various relational operations),
-  // but what it does is to figure out what might happen if all of the
-  // relations listed in relNames were joined, in terms of what it would do to
-  // the important statistics associated with the result of the join. To
-  // figure this out, the Statistics object estimates the number of tuples
-  // that would exist in the resulting relation, as well as the number of
-  // distinct values for each attribute in the resulting relation. How exactly
-  // it performs this estimation will be a topic of significant discussion in
-  // class. After this estimation is performed, all of the relations in
-  // relNames then become part of the same partition (or resulting joined
-  // relation) and no longer exist on their own.
-
-  // Note that there are a few constraints on the parameters that are input to
-  // this function. For completeness, you should probably check for violations
-  // of these constraints, because when you write your optimizer using the
-  // Statistics class, it will be very useful to have good error checking.
-
-  // First, parseTree can only list attributes that actually belong to the
-  // relations named in relNames. If any other attributes are listed, then you
-  // should probably catch this, print out an error message, and exit the
-  // program.
-  //
-  // For this, look up the numToJoin relNames in our internal relation objects
-  //           look up all the attributes in parseTree.
-  // now make sure that all the attributes are present in the internal relation object.
-
   CheckRelations(relNames, numToJoin);
-  std::vector <std::string> attrs = CheckParseTree(parseTree);
-  // if we actually return, the parse tree is good
-  //std::clog << "*** GOOD PARSE TREE!!! ***" << std::endl;
+  vector <std::string> attrs = CheckParseTree(parseTree);
   double estimate = 0;
   if (0 == parseTree and 2 >= numToJoin)
     {
@@ -254,15 +178,15 @@ void  Statistics::Apply(struct AndList *parseTree, char *relNames[], int numToJo
       rels[newRelation] = merged;
 
       { // get rid of information about old relations
-      std::vector<std::string> attrsInParseTree  = CheckParseTree(parseTree);
+      vector<std::string> attrsInParseTree  = CheckParseTree(parseTree);
       // create a set of old relations to steal attributes from.
-      std::set<string> oldRels;
+      set<string> oldRels;
       for(vector<string>::iterator it = attrsInParseTree.begin(); it < attrsInParseTree.end(); ++it)
         {
           oldRels.insert(extantAttrs[(*it)]);
         }
 
-      for(std::set<string>::const_iterator it = oldRels.begin(); it != oldRels.end(); ++it)
+      for(set<string>::const_iterator it = oldRels.begin(); it != oldRels.end(); ++it)
         {
           merged.CopyAtts(rels[*it]);
           rels.erase((*it));
@@ -274,9 +198,9 @@ void  Statistics::Apply(struct AndList *parseTree, char *relNames[], int numToJo
         }
       }
 
-      std::map<std::string, tupleCount> mergedAtts = merged.GetAtts();
+      map<std::string, tupleCount> mergedAtts = merged.GetAtts();
 
-      std::map < std::string, tupleCount>::const_iterator it;
+      map < std::string, tupleCount>::const_iterator it;
       for (it = mergedAtts.begin(); it != mergedAtts.end(); it++ )
         {
           extantAttrs[(*it).first] = newRelation;
@@ -285,33 +209,7 @@ void  Statistics::Apply(struct AndList *parseTree, char *relNames[], int numToJo
     }
 
 
-  // Second, the relations in relNames must contain exactly the set of
-  // relations in one or more of the current partitions in the Statistics
-  // object. In other words, the join specified by the set of relations in
-  // relNames must make sense. For example, imagine that there are five
-  // relations: A, B, C, D, and E, and the three current subsets maintained by
-  // the Statistics objects are {A, B}, {C, D}, and {E} (meaning that A and B
-  // have been joined, and C and D have been joined, and E is still by
-  // itself). In this case, it makes no sense if relNames contains {A, B, C},
-  // because this set contains a subset of one of the existing joins. However,
-  // relNames could contain {A, B, C, D}, or it could contain {A, B, E}, or it
-  // could contain {C, D, E}, or it could contain {A, B}, or any similar
-  // mixture of the current partitions. These are all valid, because they
-  // contain exactly those relations in one or more of the current
-  // partitions. Note that if it just contained {A, B}, then effectively we
-  // are simulating a selection.
-
-  // Also note that if parseTree is empty (that is, null), then it is assumed
-  // that there is no selection predicate; this either has no effect on the
-  // Statistics object (in the case where relNames gives exactly those
-  // relations in an existing partition) or else it specifies a pure cross
-  // product in the case that relNames combines two or more partitions.
-
-  // Finally, note that you will never be asked to write or to read from disk
-  // a Statistics object for which Apply has been called. That is, you will
-  // always write or read an object having only singleton relations.
-  //clog << endl << endl << "****************" << estimate << "******************" << endl << endl;
-}
+  }
 double Statistics::Estimate(struct AndList *parseTree, char **relNames, int numToJoin)
 {
   if (0 == parseTree and 2 >= numToJoin)
@@ -325,32 +223,10 @@ double Statistics::Estimate(struct AndList *parseTree, char **relNames, int numT
       return accumulator;
     }
 
-  using std::clog; using std::cout; using std::endl;
   CheckRelations(relNames, numToJoin);
-  std::vector <std::string> attrs = CheckParseTree(parseTree);
-  // if we actually return, the parse tree is good
-  //std::clog << "*** GOOD PARSE TREE!!! ***" << std::endl;
-
-  //clog << attrs.size() << endl;
-
-  for (unsigned int i = 0; i < attrs.size(); i++)
-    {
-      //clog << attrs[i] << endl;
-    }
-
-  // new map, to have both relations merged into it.
-  // RelationInformation merged;
-  // for (int i = 0; i < numToJoin ; i++)
-  //   {
-  //     merged.CopyAtts(rels[relNames[i]]);
-  //   }
-  // merged.print();
-
-  // check if the numdistincts in relation information is different
-  // from the number of tuples in relations
-
-  double result = CalculateEstimate(parseTree);
-  //clog << "estimated result is " << result << endl;
+  vector <std::string> attrs = CheckParseTree(parseTree);
+  
+   double result = CalculateEstimate(parseTree);
   return result;
 }
 
@@ -358,38 +234,25 @@ void Statistics :: Check (struct AndList *parseTree, char *relNames[], int numTo
 {
   CheckRelations(relNames, numToJoin);
   CheckParseTree(parseTree);
-  // if we actually return, the parse tree is good
-  //std::clog << "*** GOOD PARSE TREE!!! ***" << std::endl;
   return;
 }
 
 void Statistics :: CheckRelations(char *relNames[], int numToJoin)
 {
-  using std::string;
-  using std::clog;
-  using std::endl;
 
   for (int i = 0; i < numToJoin; i++)
     {
       string rel(relNames[i]);
-      // try for a singleton relation &
-      // try for a merged relation
-      //clog << "looking for rel " << rel << endl;
-      //clog << "single rel count" << rels.count(rel) << endl;
-      //clog << "merged rel count" << mergedRelations.count(rel) << endl;
       if (0 == rels.count(rel) and 0 == mergedRelations.count(rel))
         {
-          //clog << "relation " << rel << " not found in internal relation tracker" << endl;
           exit(-1);
         }
     }
-  //clog << "found all relations, .... now checking for all attrs in parsetree" << endl;
 }
 
-// returns a vector of the attrs
-std::vector<std::string> Statistics :: CheckParseTree(struct AndList *pAnd)
+vector<std::string> Statistics :: CheckParseTree(struct AndList *pAnd)
 {
-  std::vector < std::string > attrs;
+  vector < std::string > attrs;
   attrs.reserve(100);
   while (pAnd)
     {
@@ -404,10 +267,10 @@ std::vector<std::string> Statistics :: CheckParseTree(struct AndList *pAnd)
                 if(pOperand!=NULL && (NAME == pOperand->code))
                   {
                     // check left operand
-                    std::string attr(pOperand->value);
+                    string attr(pOperand->value);
                     if (0 == extantAttrs.count(attr))
                       {
-                        std::cerr << "left operand attribute \"" << attr << "\" not found" << std::endl;
+                        cerr << "left operand attribute \"" << attr << "\" not found" << std::endl;
                         assert(0 != extantAttrs.count(attr));
                         exit(-1);
                       }
@@ -419,10 +282,10 @@ std::vector<std::string> Statistics :: CheckParseTree(struct AndList *pAnd)
                 if(pOperand!=NULL && (NAME == pOperand->code))
                   {
                     // check right operand
-                    std::string attr(pOperand->value);
+                    string attr(pOperand->value);
                     if (0 == extantAttrs.count(attr))
                       {
-                        std::cerr << "right operand attribute \"" << attr << "\" not found" << std::endl;
+                        cerr << "right operand attribute \"" << attr << "\" not found" << std::endl;
                         assert(0 != extantAttrs.count(attr));
                         exit(-1);
                       }
@@ -449,7 +312,7 @@ double Statistics :: CalculateEstimate(AndList *pAnd)
       bool singleOR = false;
       //clog << "singleOr is " << singleOR << endl;
       { // but check
-        std::set <string> ors;
+        set <string> ors;
         unsigned count = 0;
         while (pOr) // traverse with counter.
           {
@@ -526,7 +389,7 @@ double Statistics :: CalculateEstimate(AndList *pAnd)
                         //clog << "product is " << ((double)lRelSize * (double)rRelSize) << endl;
 
                         double numerator   = lRelSize * rRelSize;
-                        double denominator = std::max(lDistinct,rDistinct);
+                        double denominator = max(lDistinct,rDistinct);
 
                         //clog << "lattr of " << lattr << " with " << lDistinct <<" distinct values is "
                              //<< "found in rel " << lrel << " of size " << lRelSize << endl;
@@ -632,10 +495,10 @@ double Statistics :: CalculateEstimate(AndList *pAnd)
                 if(pOperand!=NULL and (NAME == pOperand->code))
                   {
                     // check left operand
-                    std::string attr(pOperand->value);
+                    string attr(pOperand->value);
                     if (0 == extantAttrs.count(attr))
                       {
-                        std::cerr << "estimate left operand attribute \"" << attr << "\" not found" << std::endl;
+                        cerr << "estimate left operand attribute \"" << attr << "\" not found" << std::endl;
                         assert(0 != extantAttrs.count(attr));
                         exit(-1);
                       }
@@ -647,10 +510,10 @@ double Statistics :: CalculateEstimate(AndList *pAnd)
                 if(pOperand!=NULL and (NAME == pOperand->code))
                   {
                     // check right operand
-                    std::string attr(pOperand->value);
+                    string attr(pOperand->value);
                     if (0 == extantAttrs.count(attr))
                       {
-                        std::cerr << "estimate right operand attribute \"" << attr << "\" not found" << std::endl;
+                        cerr << "estimate right operand attribute \"" << attr << "\" not found" << std::endl;
                         assert(0 != extantAttrs.count(attr));
                         exit(-1);
                       }
@@ -695,7 +558,7 @@ bool Statistics :: HasJoin(AndList *pAnd)
       bool singleOR = false;
       //clog << "singleOr is " << singleOR << endl;
       { // but check
-        std::set <string> ors;
+        set <string> ors;
         unsigned count = 0;
         while (pOr) // traverse with counter.
           {
@@ -772,7 +635,7 @@ bool Statistics :: HasJoin(AndList *pAnd)
                         //clog << "product is " << ((double)lRelSize * (double)rRelSize) << endl;
 
                         double numerator   = lRelSize * rRelSize;
-                        double denominator = std::max(lDistinct,rDistinct);
+                        double denominator = max(lDistinct,rDistinct);
 
                         //clog << "lattr of " << lattr << " with " << lDistinct <<" distinct values is "
                              //<< "found in rel " << lrel << " of size " << lRelSize << endl;
@@ -878,10 +741,10 @@ bool Statistics :: HasJoin(AndList *pAnd)
                 if(pOperand!=NULL and (NAME == pOperand->code))
                   {
                     // check left operand
-                    std::string attr(pOperand->value);
+                    string attr(pOperand->value);
                     if (0 == extantAttrs.count(attr))
                       {
-                        std::cerr << "operand attribute \"" << attr << "\" not found" << std::endl;
+                        cerr << "operand attribute \"" << attr << "\" not found" << std::endl;
                         assert(0 != extantAttrs.count(attr));
                         exit(-1);
                       }
@@ -893,10 +756,10 @@ bool Statistics :: HasJoin(AndList *pAnd)
                 if(pOperand!=NULL and (NAME == pOperand->code))
                   {
                     // check right operand
-                    std::string attr(pOperand->value);
+                    string attr(pOperand->value);
                     if (0 == extantAttrs.count(attr))
                       {
-                        std::cerr << "operand attribute \"" << attr << "\" not found" << std::endl;
+                        cerr << "operand attribute \"" << attr << "\" not found" << std::endl;
                         assert(0 != extantAttrs.count(attr));
                         exit(-1);
                       }
